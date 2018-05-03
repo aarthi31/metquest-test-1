15b21317e6b664d042eb4d6fbb5317324fd1d95c
Aarthi Ravikrishnan <bt13d031@smail.iitm.ac.in>
2018-05-03 21:36:30 +0530

    Conf files python

--- metquest/docs/make.bat
+++ metquest/docs/make.bat
@@ -7,9 +7,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=source
-set BUILDDIR=build
-set SPHINXPROJ=metquest
+set SOURCEDIR=.
+set BUILDDIR=_build
+set SPHINXPROJ=Metquest
 
 if "%1" == "" goto help
 
